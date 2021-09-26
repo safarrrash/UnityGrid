b48bebfe17de7a9c37f06cb46c34ded6e86506bc
safarrrash <safar.rrash47@outlook.com>
2021-09-26 20:48:38 +0300

    Created Basics of Placing Mode

--- Small Run/Assets/Scripts/GameManager.cs
+++ Small Run/Assets/Scripts/GameManager.cs
@@ -7,18 +7,62 @@ public class GameManager : MonoBehaviour
     Ray ray;
     RaycastHit2D hit;
 
+    public GameObject WhiteSquare;
+
+    private bool PlacingMode;
+    [SerializeField] private GameObject defaultTile;
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if(isInPlacingMode()) enterPlacingMode();
+
+        if (Input.GetKeyDown(KeyCode.O)) togglePlacingMode(true);
+        if (Input.GetKeyDown(KeyCode.K)) togglePlacingMode(false);
+
+    }
+
+    void enterPlacingMode()
+    {
+        if (GetHitGameObject().GetComponent<TileManager>().isPlacable() && GetHitGameObject() != WhiteSquare)
         {
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
-            if (hit)
-            {
-                GameObject hitGO = hit.collider.gameObject;
-                print(hitGO.GetComponent<TileManager>().isPlacable());
-            }
+            GameObject selectedTile = GetHitGameObject();
+            WhiteSquare.transform.position = selectedTile.transform.position;
         }
+        else
+        {
+            WhiteSquare.transform.position = defaultTile.transform.position;
+        }
+    }
+
+    GameObject GetHitGameObject()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+        if (!hit)
+        {
+            Debug.LogWarning("Could Not Return Hit Object");
+            return defaultTile;
+
+        }
+        else
+        {
+            return hit.collider.gameObject;
+        }
+    }
+
+    void togglePlacingMode(bool isEnabled)
+    {
+        if (isEnabled) PlacingMode = true;
+        else if (!isEnabled)
+        {
+            PlacingMode = false;
+            WhiteSquare.transform.position = defaultTile.transform.position;
+        }
+    }
+
+    bool isInPlacingMode()
+    {
+        return PlacingMode;
     }
 
 }
